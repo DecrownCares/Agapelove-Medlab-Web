6a33cc8ccf450bca42b72d237ac3d6478a770bdf
DecrownCares <154242377+DecrownCares@users.noreply.github.com>
2025-07-24 16:18:05 +0100

    Update index.js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         try {
             // Send request to the server
-            const response = await fetch("https://agapelove-medlab-ms.onrender.com/api/appointments/book", {
+            const response = await fetch("https://agapelove-medlab-ms-hxer.onrender.com/api/appointments/book", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
@@ -124,7 +124,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     
         reviews.forEach(review => {
-            const reviewImage = `https://agapelove-medlab-ms.onrender.com${review.image}`
+            const reviewImage = `https://agapelove-medlab-ms-hxer.onrender.com${review.image}`
             const testimonialHTML = `
                 <div class="testimonial-item">
                     <div class="icon-box-primary mb-4">
